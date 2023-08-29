2ecb941c4610a6c4267b1184b5c4f014b1806555
RodolfoLobos <stevenlovoz3@gmail.com>
2023-08-29 10:13:55 -0600

    Números fibonacci

---
+++ Numero-fibinacci/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Numero_fibinacci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblFibinacci = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(85, 214);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(113, 30);
+            this.btnProcesar.TabIndex = 0;
+            this.btnProcesar.Text = "Proceso";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.Location = new System.Drawing.Point(64, 142);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(162, 35);
+            this.txtCantidad.TabIndex = 1;
+            // 
+            // lblFibinacci
+            // 
+            this.lblFibinacci.AutoSize = true;
+            this.lblFibinacci.Location = new System.Drawing.Point(299, 112);
+            this.lblFibinacci.Name = "lblFibinacci";
+            this.lblFibinacci.Size = new System.Drawing.Size(0, 20);
+            this.lblFibinacci.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFibinacci);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.btnProcesar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblFibinacci;
+    }
+}
+
---
+++ Numero-fibinacci/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numero_fibinacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            int cant = int.Parse(txtCantidad.Text);
+            int Cbase = 0;
+            int resu = 1;
+            int tempo;
+
+            for (int i = 0; i < cant; i++) ;
+            {
+                lblFibinacci.Text = lblFibinacci + Cbase.ToString() + " ,"; //0 1 1 2 3 5 
+                tempo = Cbase;// 0 1 1 2 3 5 
+                Cbase = Cbase + resu;//1 1 2 3 5
+                resu = tempo;//1 1 2 3 5 
+
+            }
+        }
+    }
+}
